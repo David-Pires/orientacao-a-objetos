b5c05758159d5d508a2a3f1811bf9123272136cf
David_Pires <developer.david@outlook.com>
2021-12-27 14:25:40 -0300

    instanciando classes

---
+++ index.js
@@ -0,0 +1,36 @@
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+  
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+
+
+ cliente1.nome = 'Ricardo';
+ cliente1.cpf = 11122233309;
+ cliente1.agencia = 1001;
+ cliente1.saldo = 0;
+ 
+
+cliente2.nome = 'Alice';
+cliente2.cpf = 22222233310;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+
+const cliente3Nome = 'Adolfo';
+const cliente3Cpf = 5552233310;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+
+
+
+console.log(cliente1);
+console.log(cliente2);
+
